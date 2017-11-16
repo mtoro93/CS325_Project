998091b7345fd9d7de9a7c3b64addbd90285796e
welleram <welleram@oregonstate.edu>
2017-11-16 12:41:16 -0800

    corrected off by 1 error

--- src/main.cpp
+++ src/main.cpp
@@ -41,7 +41,7 @@ using namespace std;
 	 
 	 //place in outputFile by looping through vector 
 	 int size = C.size();
-	 	for (int i = 0; i < size-1; i++) 
+	 	for (int i = 0; i < size; i++) 
 		{
 			outputFile << C[i].cityID;
 			outputFile << " ";
